cf72c16d5e3582103c2e588d9f0d081e7aa7cdae
Denis Jefferson <denisdorohin23@gmail.com>
2024-06-05 13:17:12 +0300

    Добавьте файлы проекта.

---
+++ Курсач/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Курсач
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.size_Array = new System.Windows.Forms.TextBox();
+            this.Button_general_array = new System.Windows.Forms.Button();
+            this.Button_sort = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radio_Bubble_sort = new System.Windows.Forms.RadioButton();
+            this.radio_Quick_sort = new System.Windows.Forms.RadioButton();
+            this.Button_array_to_File = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Buttom_WriteArrayToFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // size_Array
+            // 
+            this.size_Array.Location = new System.Drawing.Point(312, 189);
+            this.size_Array.Name = "size_Array";
+            this.size_Array.Size = new System.Drawing.Size(182, 27);
+            this.size_Array.TabIndex = 1;
+            this.size_Array.Text = " Розмір масиву";
+            this.size_Array.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Button_general_array
+            // 
+            this.Button_general_array.Location = new System.Drawing.Point(339, 222);
+            this.Button_general_array.Name = "Button_general_array";
+            this.Button_general_array.Size = new System.Drawing.Size(131, 57);
+            this.Button_general_array.TabIndex = 3;
+            this.Button_general_array.Text = "Згенерувати масив";
+            this.Button_general_array.UseVisualStyleBackColor = true;
+            this.Button_general_array.Click += new System.EventHandler(this.Button_general_array_Click);
+            // 
+            // Button_sort
+            // 
+            this.Button_sort.Location = new System.Drawing.Point(657, 218);
+            this.Button_sort.Name = "Button_sort";
+            this.Button_sort.Size = new System.Drawing.Size(131, 57);
+            this.Button_sort.TabIndex = 4;
+            this.Button_sort.Text = "Сортувати";
+            this.Button_sort.UseVisualStyleBackColor = true;
+            this.Button_sort.Click += new System.EventHandler(this.Button_sort_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 109);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.TabStop = false;
+            // 
+            // radio_Bubble_sort
+            // 
+            this.radio_Bubble_sort.AutoSize = true;
+            this.radio_Bubble_sort.Location = new System.Drawing.Point(540, 252);
+            this.radio_Bubble_sort.Name = "radio_Bubble_sort";
+            this.radio_Bubble_sort.Size = new System.Drawing.Size(111, 23);
+            this.radio_Bubble_sort.TabIndex = 7;
+            this.radio_Bubble_sort.TabStop = true;
+            this.radio_Bubble_sort.Text = "Bubble Sort";
+            this.radio_Bubble_sort.UseVisualStyleBackColor = true;
+            // 
+            // radio_Quick_sort
+            // 
+            this.radio_Quick_sort.AutoSize = true;
+            this.radio_Quick_sort.Location = new System.Drawing.Point(540, 223);
+            this.radio_Quick_sort.Name = "radio_Quick_sort";
+            this.radio_Quick_sort.Size = new System.Drawing.Size(104, 23);
+            this.radio_Quick_sort.TabIndex = 8;
+            this.radio_Quick_sort.TabStop = true;
+            this.radio_Quick_sort.Text = "Quick Sort";
+            this.radio_Quick_sort.UseVisualStyleBackColor = true;
+            // 
+            // Button_array_to_File
+            // 
+            this.Button_array_to_File.Location = new System.Drawing.Point(12, 219);
+            this.Button_array_to_File.Name = "Button_array_to_File";
+            this.Button_array_to_File.Size = new System.Drawing.Size(131, 56);
+            this.Button_array_to_File.TabIndex = 9;
+            this.Button_array_to_File.Text = "Взяти масив з файлу";
+            this.Button_array_to_File.UseVisualStyleBackColor = true;
+            this.Button_array_to_File.Click += new System.EventHandler(this.Button_array_to_File_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(264, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 32);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Cортування масиву";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 19);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Шлях до файлу";
+            // 
+            // Buttom_WriteArrayToFile
+            // 
+            this.Buttom_WriteArrayToFile.Location = new System.Drawing.Point(171, 223);
+            this.Buttom_WriteArrayToFile.Name = "Buttom_WriteArrayToFile";
+            this.Buttom_WriteArrayToFile.Size = new System.Drawing.Size(98, 48);
+            this.Buttom_WriteArrayToFile.TabIndex = 15;
+            this.Buttom_WriteArrayToFile.Text = "Запись в файл";
+            this.Buttom_WriteArrayToFile.UseVisualStyleBackColor = true;
+            this.Buttom_WriteArrayToFile.Click += new System.EventHandler(this.Button_WriteArrayToFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 19);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Записать в файл";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 310);
+            this.Controls.Add(this.Buttom_WriteArrayToFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Button_array_to_File);
+            this.Controls.Add(this.radio_Quick_sort);
+            this.Controls.Add(this.radio_Bubble_sort);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Button_sort);
+            this.Controls.Add(this.Button_general_array);
+            this.Controls.Add(this.size_Array);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox size_Array;
+        private System.Windows.Forms.Button Button_general_array;
+        private System.Windows.Forms.Button Button_sort;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radio_Bubble_sort;
+        private System.Windows.Forms.RadioButton radio_Quick_sort;
+        private System.Windows.Forms.Button Button_array_to_File;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Buttom_WriteArrayToFile;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Курсач/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Курсач
+{
+    public partial class Form1 : Form
+    {
+        private SortArray sortArray = new SortArray();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_array_to_File_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string line = File.ReadAllText(openFileDialog.FileName);
+
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        MessageBox.Show("Файл пуст.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    string[] numbers = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int[] array = Array.ConvertAll(numbers, int.Parse);
+                    sortArray.setArray(array);
+
+                    UpdateDataGridView(array);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при считывании файла: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Button_general_array_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = int.Parse(size_Array.Text);
+                Random random = new Random();
+                int[] array = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = random.Next(100);
+                }
+
+                sortArray.setArray(array);
+                UpdateDataGridView(array);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите корректный размер массива.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button_sort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radio_Bubble_sort.Checked)
+                {
+                    sortArray.bubbleSort();
+                }
+                else if (radio_Quick_sort.Checked)
+                {
+                    sortArray.quickSort();
+                }
+
+                int[] sortedArray = sortArray.getArray();
+                UpdateDataGridView(sortedArray);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сортировке массива: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateDataGridView(int[] array)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                dataGridView1.Columns.Add($"ArrayColumn{i}", $"{i + 1}");
+            }
+
+            dataGridView1.Rows.Add();
+            for (int i = 0; i < array.Length; i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = array[i];
+            }
+
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ReadOnly = true;
+        }
+        private void Button_WriteArrayToFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    int[] array = sortArray.getArray();
+                    string content = string.Join(" ", array);
+                    File.WriteAllText(saveFileDialog.FileName, content);
+                    MessageBox.Show("Массив успешно записан в файл.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при записи файла: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public class SortArray
+        {
+            public int[] array { get; private set; }
+
+            public SortArray()
+            {
+                array = new int[0];
+            }
+
+            public SortArray(int[] array)
+            {
+                this.array = array;
+            }
+
+            public void setArray(int[] array)
+            {
+                this.array = array;
+            }
+
+            public int[] getArray()
+            {
+                return array;
+            }
+
+            public void bubbleSort()
+            {
+                int n = array.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            public void quickSort()
+            {
+                quickSort(0, array.Length - 1);
+            }
+
+            private void quickSort(int low, int high)
+            {
+                if (low < high)
+                {
+                    int pi = Partition(low, high);
+                    quickSort(low, pi - 1);
+                    quickSort(pi + 1, high);
+                }
+            }
+
+            private int Partition(int low, int high)
+            {
+                int pivot = array[high];
+                int i = low - 1;
+                for (int j = low; j < high; j++)
+                {
+                    if (array[j] < pivot)
+                    {
+                        i++;
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+                int temp1 = array[i + 1];
+                array[i + 1] = array[high];
+                array[high] = temp1;
+                return i + 1;
+            }
+
+            public void printArray()
+            {
+                foreach (int i in array)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
